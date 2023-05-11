9473325853d71603f3dff633088866130a7b6e14
Shoaib Latif <shoaiblatif11@gmail.com>
2023-05-11 22:41:39 +0100

    Completed 2nd exercise

--- codewars_fundamentals/codewars.rb
+++ codewars_fundamentals/codewars.rb
@@ -3,3 +3,9 @@ def sum_two_smallest_numbers(numbers)
   smallest_numbers = numbers.sort
   smallest_numbers[0] + smallest_numbers[1]
 end
+
+## Q2 ##
+def twice_as_old(dad, son)
+  age_difference = dad - 2 * son
+  age_difference.abs
+end

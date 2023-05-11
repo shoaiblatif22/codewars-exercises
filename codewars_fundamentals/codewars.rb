91917e5e3bf03dc1a74fd0411948b0b1c1024a32
Shoaib Latif <shoaiblatif11@gmail.com>
2023-05-11 23:21:18 +0100

    Completed exercise 3 and exercise 4

--- codewars_fundamentals/codewars.rb
+++ codewars_fundamentals/codewars.rb
@@ -9,3 +9,32 @@ def twice_as_old(dad, son)
   age_difference = dad - 2 * son
   age_difference.abs
 end
+
+## Q3 ##
+def rps(p1, p2)
+  beatmap = {'scissors' => 'paper', 'paper' => 'rock', 'rock' => "scissors"}
+
+  if p1 == p2
+    "Draw!"
+  elsif beatmap[p1] == p2
+    "Player 1 won!"
+  else
+    "Player 2 won!"
+  end
+end
+
+## Q4 ##
+def find_difference(a, b)
+  #take two lists of integers => a and => b
+  #Each list will consist of 3 positive ints > 0 
+  
+  cuboid_one = a 
+  cuboid_two = b 
+  
+  #vol = width * length * height
+  
+  volume_a = cuboid_one[0] * cuboid_one[1] * cuboid_one[2]
+  volume_b = cuboid_two[0] * cuboid_two[1] * cuboid_two[2]
+  
+  volume_difference = (volume_a - volume_b).abs 
+end
